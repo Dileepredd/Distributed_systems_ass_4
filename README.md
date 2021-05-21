4884e53d795e022f766db844966d5113f39a18e3
Dileepredd <cs17b034@iittp.ac.in>
2021-05-21 23:45:29 +0530

    Distributed systems assignment 4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### when ever executed provides latest news in terms of headlines.
+### output will be on terminal.
 ### uses ```wionews.com``` as a source for scrapping.
--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main(){
+	count := 1
 	collector := colly.NewCollector(
 		colly.AllowedDomains("wionews.com","www.wionews.com"),
 	)
 	collector.OnHTML(".content-holder h1 a,.content-holder h2 a,.content-holder  h3 a,.content-holder  h4 a,.content-holder  h5 a,.content-holder  h6 a",func (element *colly.HTMLElement){
-		fmt.Printf("%v\n",element.Text)
+		fmt.Printf("%v: %v\n",count,element.Text)
+		count +=1
 	})
 
 	collector.OnRequest(func (request *colly.Request){
-		fmt.Println("visiting %v",request.URL.String())
+		fmt.Println("visiting ",request.URL.String())
 	})
 
 	collector.Visit("https://www.wionews.com/")
